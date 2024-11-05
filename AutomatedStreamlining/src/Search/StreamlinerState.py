2e94edf32efcdf0722b8b0bb8e56ab2736b780f4
Orhan Yiğit Yazıcılar <yigityazicilar@gmail.com>
2024-11-05 16:37:09 +0000

    Added scopes to the streamliners now the streamliners cannot be combined if they have the same scope and an overlapping group.

--- AutomatedStreamlining/src/Search/StreamlinerState.py
+++ AutomatedStreamlining/src/Search/StreamlinerState.py
@@ -1,60 +1,61 @@
-import json
+import json, sys
 import logging
+from logging import config
 from typing import AnyStr, Set, List, FrozenSet, Dict, Any
 
 from StreamlinerSelection import StreamlinerSelection
 
-selected_streamliner_groups = ["MatrixByRowBucket-0", "MatrixByRowBucket-9"]
-removed_streamliner_groups = ["MatrixBy"]
-
 
 class StreamlinerState:
+    SELECTED_GROUPS = ["MatrixByRowBucket-0", "MatrixByRowBucket-9"]
+    SCOPES = ["MatrixByRowBucket", "MatrixByColBucket"]
+
     def __init__(
         self, streamliner_output: AnyStr, streamliners_to_use: StreamlinerSelection
-    ):
-        self.streamliner_json: Dict[str, Any] = json.loads(streamliner_output)
-        temp_streamliner_dict: Dict[str, Any] = {}
-
-        if streamliners_to_use == StreamlinerSelection.FINE_FILTERED:
-            for k, v in self.streamliner_json.items():
-                for s in selected_streamliner_groups:
-                    if s in v["groups"]:
-                        v["groups"].remove(s)
-                        temp_streamliner_dict[k] = v
-        elif streamliners_to_use == StreamlinerSelection.COARSE:
-            for k, v in self.streamliner_json.items():
-                for r in removed_streamliner_groups:
-                    filtered = False
-                    for g in v["groups"]:
-                        if r in g:
-                            filtered = True
-                            break
-                    if not filtered:
-                        temp_streamliner_dict[k] = v
-        else:
-            for k, v in self.streamliner_json.items():
-                for r in removed_streamliner_groups:
-                    filtered = False
-                    for g in v["groups"]:
-                        if r in g:
-                            filtered = True
-                            break
-                    if filtered:
-                        temp_streamliner_dict[k] = v
-
-        self.streamliner_json = temp_streamliner_dict
+    ) -> None:
+        raw_streamliners = json.loads(streamliner_output)
+        scope_streamliners = self._change_to_scope(raw_streamliners)
+        self.streamliner_json = self._filter_streamliners(
+            scope_streamliners, streamliners_to_use
+        )
         logging.info(f"Number of candidate streamliners: {len(self.streamliner_json)}")
-        self.init_groups(self.streamliner_json)
         self.invalid_combinations: Set[FrozenSet[str]] = set()
 
-    def init_groups(self, streamliner_json: Dict[str, Any]) -> None:
-        self.groups = {}
-        for val in streamliner_json:
-            for group in streamliner_json[val]["groups"]:
-                if group in self.groups:
-                    self.groups[group].add(val)
-                else:
-                    self.groups[group] = set(val)
+    def _change_to_scope(self, streamliners: Dict[str, Any]) -> Dict[str, Any]:
+        for id, config in streamliners.items():
+            for group in config["groups"]:
+                for change in self.SCOPES:
+                    if change in group:
+                        config["groups"].remove(group)
+                        config["scope"] = group
+                        break
+                if config.get("scope", None) is None:
+                    config["scope"] = "MatrixWide"
+
+            streamliners[id] = config
+
+        return streamliners
+
+    def _filter_streamliners(
+        self, streamliners: Dict[str, Any], mode: StreamlinerSelection
+    ) -> Dict[str, Any]:
+        filtered = {}
+        match mode:
+            case StreamlinerSelection.FINE_FILTERED:
+                for id, config in streamliners.items():
+                    if config["scope"] in self.SELECTED_GROUPS:
+                        filtered[id] = config
+            case StreamlinerSelection.COARSE:
+                for id, config in streamliners.items():
+                    if config["scope"] == "MatrixWide":
+                        filtered[id] = config
+            case StreamlinerSelection.FINE:
+                for id, config in streamliners.items():
+                    if config["scope"] != "MatrixWide":
+                        filtered[id] = config
+            case StreamlinerSelection.ALL:
+                filtered = streamliners
+        return filtered
 
     def get_candidate_streamliners(self) -> Set[str]:
         return set(self.streamliner_json.keys())
@@ -62,45 +63,47 @@ class StreamlinerState:
     def get_possible_adjacent_streamliners(
         self, streamliner_combination: Set[str]
     ) -> Set[str]:
-        possible_adjacent_streamliners: Set[str] = (
+        candidates: Set[str] = (
             self.get_candidate_streamliners() - streamliner_combination
         )
-        to_remove: Set[str] = set()
+        invalid_candidates: Set[str] = set()
+
         for streamliner in streamliner_combination:
-            groups: List[str] = self.streamliner_json[streamliner]["groups"]
+            current_scope: str = self.streamliner_json[streamliner]["scope"]
+            current_groups = set(self.streamliner_json[streamliner]["groups"])
 
-            for candidate_streamliner in possible_adjacent_streamliners:
-                if not set(groups) - set(
-                    self.streamliner_json[candidate_streamliner]["groups"]
+            for candidate in candidates:
+                candidate_groups = set(self.streamliner_json[candidate]["groups"])
+                candidate_scope = self.streamliner_json[candidate]["scope"]
+                if (
+                    len(current_groups.intersection(candidate_groups)) != 0
+                    and current_scope == candidate_scope
                 ):
-                    to_remove.add(candidate_streamliner)
+                    invalid_candidates.add(candidate)
 
-                current_set: Set[str] = set.union(
-                    set(candidate_streamliner), streamliner_combination
-                )
-                if set.intersection(current_set, self.invalid_combinations):
-                    to_remove.add(candidate_streamliner)
+                # Check if combination would be invalid
+                combination: Set[str] = set([candidate]) | streamliner_combination
+                if any(
+                    invalid_set == combination
+                    for invalid_set in self.invalid_combinations
+                ):
+                    invalid_candidates.add(candidate)
 
-        possible_adjacent_streamliners.difference_update(to_remove)
-        return possible_adjacent_streamliners
+        return candidates - invalid_candidates
 
     def get_possible_adjacent_combinations(
-        self, current_streamliner_combination: Set[str]
+        self, current_combination: Set[str]
     ) -> Set[str]:
-        adjacent_streamliners: Set[str] = self.get_possible_adjacent_streamliners(
-            current_streamliner_combination
+        adjacent_streamliners = self.get_possible_adjacent_streamliners(
+            current_combination
         )
-        return set(
-            [
-                self.get_streamliner_repr_from_set(
-                    current_streamliner_combination.union(set([streamliner]))
-                )
-                for streamliner in adjacent_streamliners
-            ]
-        )
+        return {
+            self.get_streamliner_repr_from_set(current_combination | {streamliner})
+            for streamliner in adjacent_streamliners
+        }
 
     def get_streamliner_repr_from_set(self, streamliner_combo: Set[str]) -> str:
-        return "-".join([str(i) for i in sorted([int(s) for s in streamliner_combo])])
+        return "-".join(str(i) for i in sorted(int(s) for s in streamliner_combo))
 
     def add_invalid_combination(self, combination: Set[str]) -> None:
         logging.info("Adding invalid combination")
--- AutomatedStreamlining/src/StreamlinerSelection.py
+++ AutomatedStreamlining/src/StreamlinerSelection.py
@@ -5,6 +5,7 @@ class StreamlinerSelection(Enum):
     COARSE = "coarse"
     FINE = "fine"
     FINE_FILTERED = "fine_filtered"
+    ALL = "all"
 
     def __str__(self):
         return self.value
