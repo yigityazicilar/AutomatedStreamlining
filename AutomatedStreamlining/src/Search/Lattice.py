56bc935ff8309337561269a394c6806896489d9f
Orhan Yiğit Yazıcılar <yigityazicilar@gmail.com>
2024-11-06 16:11:05 +0000

    Removing the LatticeNode class as it is not used.

--- AutomatedStreamlining/src/Search/Lattice.py
+++ AutomatedStreamlining/src/Search/Lattice.py
@@ -3,29 +3,6 @@ import logging
 from networkx import DiGraph
 
 
-class LatticeNode:
-    def __eq__(self, o: object) -> bool:
-        return (
-            isinstance(o, LatticeNode)
-            and o.streamliner_combination() == self._streamliner_combination
-        )
-
-    def __str__(self) -> str:
-        return ",".join(sorted([x for x in self._streamliner_combination]))
-
-    """ Return the hash of the sorted individual streamliners"""
-
-    def __hash__(self) -> int:
-        return self._streamliner_combination.__hash__()
-
-    def __init__(self, streamliner_combination: Set[str]):
-        self.children = {}
-        self._streamliner_combination = frozenset(streamliner_combination)
-
-    def streamliner_combination(self):
-        return self._streamliner_combination
-
-
 class Lattice:
     def __init__(self):
         self.__graph: DiGraph = DiGraph()
